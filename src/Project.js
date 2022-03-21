1190e46f1f3a3379b1de4155a79e9c594876d873
HuberAdrian <huberadrian@web.de>
2022-03-21 14:24:07 +0100

    refactored scope of link

--- src/Project.js
+++ src/Project.js
@@ -6,11 +6,14 @@ function Project(props) {
 
   return (
     <>
-      <a className='project' href={link} rel="noopener noreferrer" target="_blank">
+    <div className='project'>
+      <a href={link} rel="noopener noreferrer" target="_blank">
         <img className='image_preview' src={image}></img>
         <h3 className='h3_preview' >{name}</h3>
-        <Collapsible />
       </a>
+        <Collapsible />
+      
+    </div>
     </>
   )
 }
