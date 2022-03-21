61b0f45faf012f239a0203d2342fa6028ba80086
HuberAdrian <huberadrian@web.de>
2022-03-21 10:13:14 +0100

    added Images, added css

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
       <h1>Programming Projects</h1>
       <h2>Web Development</h2>
       <div>
-        <Project name="" link="" image="" learned={[]} />
-        <Project name="Project1" link="https://www.google.com" image="" learned={[]} />
+        <Project name="Online Shop" link="" image="Images/Online_Shop.png" learned={[]} />
+        <Project name="Food Tracker" link="https://www.google.com" image="" learned={[]} used_technologies="" description="" />
       </div>
 
     </div>
--- src/Project.js
+++ src/Project.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 function Project(props) {
-    const {name, link, image, learned} = props
+    const {name, link, image, learned, used_technologies, description} = props
 
   return (
     <>
       <a href={link} rel="noopener noreferrer" target="_blank">
-        <img src={image}></img>
-        <h2>{name}</h2>
+        <img className='image_preview' src={image}></img>
+        <h3 className='h3_preview' >{name}</h3>
       </a>
     </>
   )
