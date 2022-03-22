7c5fad3ebf64032e035490b21079564968e32936
HuberAdrian <huberadrian@web.de>
2022-03-22 15:57:46 +0100

    added Technologies used for websites

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
       <h1>Programming Projects</h1>
       <h2>Web Development</h2>
       <div className='flex_container'>
-        <Project name="Online Shop" link="http://online-shop.adrian-huber.org" image="Images/Online_Shop.png" description="" used_technologies={[]} />
-        <Project name="Copilot for Web Development" link="" image="Images/Co_Pilot.png" description="" used_technologies={[]} />
-        <Project name="Food Tracker" link="https://www.google.com" image="Images/Food_Tracker.png" used_technologies={["React", "HTML", "CSS", "JS"]} description="" />
+        <Project name="Online Shop" link="http://online-shop.adrian-huber.org" image="Images/Online_Shop.png" description="" used_technologies={["React", "Redux", "Jest (testing Library)", "React-router-dom", "Stripe API", "HTML", "CSS", "JavaScript"]} />
+        <Project name="Copilot for Web Development" link="" image="Images/Co_Pilot.png" description="" used_technologies={["React", "react-notion (Libary to include Notion pages)", "HTML", "CSS", "JavaScript", ]} />
+        <Project name="Food Tracker" link="https://www.google.com" image="Images/Food_Tracker.png" used_technologies={["React", "Chart.js", "HTML", "CSS", "JavaScript"]} description="" />
       </div>
       <h2>Data Science</h2>
 
