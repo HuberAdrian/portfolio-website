390cbb99cb40b430112a21f920c8d56f7c02cc91
HuberAdrian <huberadrian@web.de>
2022-03-22 10:07:36 +0100

    changed pictures, added Data Science header

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ function App() {
       <h1>Programming Projects</h1>
       <h2>Web Development</h2>
       <div className='flex_container'>
-        <Project name="Online Shop" link="" image="Images/Online_Shop.png" learned={[]} />
+        <Project name="Online Shop" link="http://online-shop.adrian-huber.org" image="Images/Online_Shop.png" learned={[]} />
         <Project name="Copilot for Web Development" link="" image="Images/Co_Pilot.png" learned={[]} />
         <Project name="Food Tracker" link="https://www.google.com" image="Images/Food_Tracker.png" learned={[]} used_technologies="" description="" />
       </div>
+      <h2>Data Science</h2>
 
     </div>
   );
